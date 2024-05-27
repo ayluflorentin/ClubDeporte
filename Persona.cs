3d0eb7460174ebf6fa6bdf9c2a665241ced23cc1
vzblea <elianavazquez50@gmail.com>
2024-05-27 17:35:41 -0300

    Create Persona.cs

---
+++ Persona.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+namespace ClubDeportes
+{   
+	public class ClubDeportivo
+    { //representará a todas las personas asociadas al club deportivo
+        private List<Persona> personas;
+        public ClubDeportivo()
+        {
+            personas = new List<Persona>();
+        }
+
+	public class Persona
+    {
+        // Campos protegidos para poder acceder desde otras clases
+        protected string nombre;
+        protected string dni;
+        protected int edad;
+
+        // Propiedades públicas para acceso a los campos privados
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Dni
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        // Constructor que inicializa todos los campos
+        public Persona(string nombre, string dni, int edad)
+        {
+            this.nombre = nombre;
+            this.dni = dni;
+            this.edad = edad;
+        }
+       //metodo
+        public string ObtenerInformacion()
+    {
+        return "Nombre: " + nombre + " Dni: " + dni +" Edad: "+ edad;
+    }
+    
